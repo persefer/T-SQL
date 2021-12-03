15e6d99775b6e27e53d3e3019eecbe97fe95d9a1
persefer <persefer@hotmail.com>
2021-12-03 16:50:03 +0300

    Create aggregations.sql

---
+++ aggregations.sql
@@ -0,0 +1,22 @@
+-- We can calculate totals, subtotals and grand subtotals with the script below;
+
+WITH SampleData AS
+(
+    SELECT [Month] = 'October' , Category = 'A', Amount = 1000 UNION ALL
+    SELECT [Month] = 'October' , Category = 'A', Amount = 500  UNION ALL
+    SELECT [Month] = 'November', Category = 'A', Amount = 750  UNION ALL
+    SELECT [Month] = 'November', Category = 'A', Amount = 1250 UNION ALL
+    SELECT [Month] = 'October' , Category = 'B', Amount = 450  UNION ALL
+    SELECT [Month] = 'October' , Category = 'B', Amount = 650  UNION ALL
+    SELECT [Month] = 'November', Category = 'B', Amount = 800  UNION ALL
+    SELECT [Month] = 'October' , Category = 'C', Amount = 100  UNION ALL
+    SELECT [Month] = 'November', Category = 'C', Amount = 350
+)
+SELECT
+     [Month]
+    ,Category
+    ,Amount         = SUM(Amount)
+    ,SubTotalFlag   = GROUPING([Category])
+    ,GrandTotalFlag = GROUPING([Month])
+FROM [SampleData]
+GROUP BY ROLLUP([Month],[Category]);
